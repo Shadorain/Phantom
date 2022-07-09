6d30d680151b1e3cabdf1904312440eeebd5b7a0
Shadorain <jordanf7517@gmail.com>
2022-07-09 15:47:04 -0400

    Gonna use crossterm and port from termion

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,11 @@
+mod terminal;
+
+pub const NAME: &str =  env!("CARGO_PKG_NAME");
+pub const AUTHOR: &str = env!("CARGO_PKG_AUTHORS");
+pub const VERSION: &str = env!("CARGO_PKG_VERSION");
+pub const DESCRIPTION: &str = env!("CARGO_PKG_DESCRIPTION");
+pub const REPOSITORY: &str = env!("CARGO_PKG_REPOSITORY");
+
 fn main() {
     println!("Hello, world!");
 }
