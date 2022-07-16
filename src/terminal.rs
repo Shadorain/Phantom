f9c1ecffbaf58c25d7f38535847820f37848b5dd
Shadorain <jordanf7517@gmail.com>
2022-07-16 12:17:27 -0400

    Refactored map_err to From, majority of functions now are builder pattern

--- src/terminal.rs
+++ src/terminal.rs
@@ -1,23 +1,27 @@
 use thiserror::Error;
 use std::io::{ Stdout, stdout, self, Write };
-use crossterm::{ cursor as c, execute, queue, terminal as t, style as s };
+use crossterm::{ cursor as c, execute, queue, terminal as t, style as s, ErrorKind };
+
+#[derive(Error, Debug)]
+pub enum TermError {
+    #[error("Failure to startup terminal")]
+    Startup,
+    #[error("Failure to cleanup terminal")]
+    Cleanup,
+    #[error("Failure to run command: {0:?}")]
+    Command(io::Error),
+}
+impl From<ErrorKind> for TermError {
+    fn from(err: ErrorKind) -> TermError {
+        TermError::Command(err)
+    }
+}
+
 
 pub struct Rgb {
     r: u8,
     g: u8,
     b: u8,
-}
-
-#[derive(Error, Debug)]
-pub enum TermError {
-    #[error("Terminal size failure")]
-    Size,
-    #[error("Failure to startup terminal")]
-    Startup,
-    #[error("Failure to cleanup terminal")]
-    Cleanup,
-    #[error("Failure to run command")]
-    Command(io::Error),
 }
 
 #[derive(Clone, Copy)]
@@ -35,7 +39,7 @@ type Result<T> = std::result::Result<T, TermError>;
 
 impl Terminal {
     pub fn new() -> Result<Self> {
-        let size = t::size().map_err(|_| TermError::Size)?;
+        let size = t::size()?;
         Ok(Self {
             size: Size { cols: size.0, rows: size.1 },
             _stdout: stdout(),
@@ -43,76 +47,101 @@ impl Terminal {
     }
 
     pub fn flush(&mut self) -> Result<()> {
-        self._stdout.flush().map_err(TermError::Command)
+        Ok(self._stdout.flush()?)
     }
-    /* Cursor */
-    pub fn cursor_hide(&mut self) -> Result<()> {
+
+
+    /* Cursor --------------------------------------------------------------- */
+
+    pub fn cursor_hide(&mut self) -> Result<&mut Self> {
         queue!(self._stdout,
             c::Hide
-        ).map_err(TermError::Command)
+        )?;
+        Ok(self)
     }
-    pub fn cursor_show(&mut self) -> Result<()> {
+    pub fn cursor_show(&mut self) -> Result<&mut Self> {
         queue!(self._stdout,
             c::Show
-        ).map_err(TermError::Command)
+        )?;
+        Ok(self)
     }
 
-    pub fn cursor_position(&mut self, col: u16, row: u16) -> Result<()> {
+    pub fn cursor_position(&mut self, col: u16, row: u16) -> Result<&mut Self> {
         queue!(self._stdout,
             c::MoveTo(col, row)
-        ).map_err(TermError::Command)
+        )?;
+        Ok(self)
     }
 
-    /* Display */
-    pub fn clear_screen(&mut self) -> Result<()> {
+
+    /* Display -------------------------------------------------------------- */
+
+    pub fn clear_screen(&mut self) -> Result<&mut Self> {
         queue!(self._stdout,
             t::Clear(t::ClearType::All)
-        ).map_err(TermError::Command)
+        )?;
+        Ok(self)
     }
-    pub fn clear_line(&mut self) -> Result<()> {
+    pub fn clear_line(&mut self) -> Result<&mut Self> {
         queue!(self._stdout,
             t::Clear(t::ClearType::CurrentLine)
-        ).map_err(TermError::Command)
+        )?;
+        Ok(self)
     }
 
-    /* Coloring */
-    pub fn fg_set(&mut self, rgb: Rgb) -> Result<()> {
+
+    /* Coloring ------------------------------------------------------------- */
+
+    pub fn fg_set(&mut self, rgb: Rgb) -> Result<&mut Self> {
         queue!(self._stdout,
             s::SetForegroundColor(s::Color::Rgb { r: rgb.r, g: rgb.g, b: rgb.b })
-        ).map_err(TermError::Command)
+        )?;
+        Ok(self)
     }
-    pub fn bg_set(&mut self, rgb: Rgb) -> Result<()> {
+    pub fn bg_set(&mut self, rgb: Rgb) -> Result<&mut Self> {
         queue!(self._stdout,
             s::SetBackgroundColor(s::Color::Rgb { r: rgb.r, g: rgb.g, b: rgb.b })
-        ).map_err(TermError::Command)
+        )?;
+        Ok(self)
     }
-    pub fn fg_reset(&mut self) -> Result<()> {
+    pub fn fg_reset(&mut self) -> Result<&mut Self> {
         queue!(self._stdout,
             s::SetForegroundColor(s::Color::Reset)
-        ).map_err(TermError::Command)
+        )?;
+        Ok(self)
     }
-    pub fn bg_reset(&mut self) -> Result<()> {
+    pub fn bg_reset(&mut self) -> Result<&mut Self> {
         queue!(self._stdout,
             s::SetBackgroundColor(s::Color::Reset)
-        ).map_err(TermError::Command)
+        )?;
+        Ok(self)
     }
 
-    /* Window Management */
+
+    /* Window Management ---------------------------------------------------- */
+
     pub fn size_get(&self) -> &Size {
         &self.size
     }
     pub fn size_set(&mut self, size: Size) -> Result<()> {
         self.size = size;
-        queue!(self._stdout,
+        Ok(queue!(self._stdout,
             t::SetSize(self.size.cols, self.size.rows)
-        ).map_err(|_| TermError::Size)
+        )?)
     }
 
-    /* Init / Deinit */
+
+    /* Init / Deinit -------------------------------------------------------- */
+
     pub fn startup(&mut self) -> Result<()> {
-        queue!(self._stdout,
+        execute!(self._stdout,
+            t::EnterAlternateScreen,
+            s::ResetColor,
+            t::Clear(t::ClearType::All),
             c::MoveTo(0, 0),
-        ).map_err(|_| TermError::Startup)
+        ).map_err(|_| TermError::Startup)?;
+
+        t::enable_raw_mode().map_err(|_| TermError::Startup)
     }
     pub fn cleanup(&mut self) -> Result<()> {
         execute!(self._stdout,
