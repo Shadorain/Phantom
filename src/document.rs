1a5abc6a52f7f2db52fc07ac502c13f72c12d099
Shadorain <jordanf7517@gmail.com>
2022-07-18 22:40:37 -0400

    +buffer.rs, Got a decent starting setup for Document. New documentation style

--- src/document.rs
+++ src/document.rs
@@ -1,14 +1,14 @@
-use std::io::ErrorKind;
-
 use thiserror::Error;
 use ropey::Rope;
 
+use std::path::Path;
+
 #[derive(Error, Debug)]
 pub enum DocError {
-    #[error("Failure to open document")]
-    Open,
-    #[error("Failure to close document")]
-    Close,
+    #[error("Err: couldn't open document: {0}")]
+    Open(String),
+    #[error("Err: couldn't close document: {0}")]
+    Close(String),
     #[error("Rope failure: {0:?}")]
     Rope(ropey::Error),
 }
@@ -20,13 +20,39 @@ impl From<ropey::Error> for DocError {
 
 type Result<T> = std::result::Result<T, DocError>;
 
+/// ┌─ Document Structure ─────────────┐
+/// │ Displays file contents, internal │
+/// │   buffer handles edits.          │
+/// │ TODO: Abstract out file IO       │
+/// │ ┌────────┐    ┌──────┐    ┌────┐ │
+/// │ │Document├───►│Buffer├───►│Rope│ │
+/// │ └───┬────┘    └──────┘◄─┐ └────┘ │
+/// │     │ ┌─ TODO ─┐   ┌────┴────┐   │
+/// │     └►│( File )├──►│BufReader│   │ 
+/// │       └────────┘   └─────────┘   │
+/// └──────────────────────────────────┘
 pub struct Document {
     pub file_name: Option<String>,
     text: Rope,
 }
 
 impl Document {
+    pub fn default() -> Self {
+        Self {
+            file_name: None,
+            text: Rope::new(),
+        }
+    }
+
     pub fn open(file_name: &str) -> Result<Self> {
-        Ok(Self { file_name, text: Rope::new() })
+        if !Document::is_valid_file(file_name) { return Err(DocError::Open(file_name.to_owned())); } 
+        Ok(Self {
+            file_name : Some(file_name.to_owned()),
+            text: Rope::new(),
+        })
+    }
+
+    pub fn is_valid_file(file_name: &str) -> bool {
+        Path::new(file_name).is_file()
     }
 }
--- src/editor.rs
+++ src/editor.rs
@@ -5,19 +5,27 @@ use crate::document::Document;
 
 pub struct Editor {
     term: Terminal,
-    buffers: Vec<Document>,
+    documents: Vec<Document>,
 }
 
 impl Editor {
     pub fn new() -> Self {
-        // let args: Vec<String> = env::args().collect();
-        // let document = if let Some(file_name) = args.get(1) {
-        //     let doc = Document::open(file_name);
-        // }
+        let args: Vec<String> = env::args().collect();
+
+        let mut doc_vec: Vec<Document> = Vec::new();
+        doc_vec.push(if let Some(file_name) = args.get(1) {
+            let doc = Document::open(file_name);
+            if let Ok(doc) = doc {
+                doc
+            } else {
+                println!("{0}", doc.is_err());
+                Document::default()
+            }
+        } else { Document::default() });
         
         Self {
             term: Terminal::new().expect("Failed to initialize terminal"),
-            buffers: Vec::new().push(Document::new(file_name)),
+            documents: doc_vec,
         }
     }
 }
